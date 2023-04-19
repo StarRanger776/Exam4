1f6604df1df49e9f8b499b7b4b4e01d8a8e98d40
StarRanger776 <89621859+StarRanger776@users.noreply.github.com>
2023-04-19 09:13:36 -0500

    Made generating problems easier?

--- Assets/Scripts/ProblemManager.cs
+++ Assets/Scripts/ProblemManager.cs
@@ -11,13 +11,13 @@ public class ProblemManager : MonoBehaviour
     public TMP_Text displayPoints;
     public TMP_InputField answer;
 
-    public int num1;
-    public int num2;
+    public int[] nums;
+    public int curAnswer;
 
     // Start is called before the first frame update
     void Start()
     {
-        GenerateProblem();
+        GenerateProblem(2, 11, 0, 0);
     }
 
     // Update is called once per frame
@@ -26,8 +26,49 @@ public class ProblemManager : MonoBehaviour
         displayPoints.text = $"Problems solved: {problemsSolved}";
     }
 
-    public void GenerateProblem()
+    public void GenerateProblem(int numNum, int highRange, int lowRange, int symbol)
     {
+        displayProblem.text = null;
+        nums = new int[numNum];
+        for (int i = 0; i < numNum; i++)
+        {
+            nums[i] = Random.Range(lowRange, highRange);
+            if(i != numNum - 1)
+            {
+                if (symbol == 0)
+                {
+                    displayProblem.text += $"{nums[i]} + ";
+                    curAnswer += nums[i];
+                }
+                if (symbol == 1)
+                {
+                    displayProblem.text += $"{nums[i]} - ";
+                    if(i == 0)
+                    {
+                        curAnswer = nums[i];
+                    }
+                    else
+                    {
+                        curAnswer -= nums[i];
+                    }
+                }
+            }
+            else
+            {
+                displayProblem.text += $"{nums[i]} =";
+                if(symbol == 0)
+                {
+                    curAnswer += nums[i];
+                }
+                else if(symbol == 1)
+                {
+                    curAnswer -= nums[i];
+                }
+            }
+        }
+
+
+        /*
         if(problemsSolved <= 5)
         {
             num1 = Random.Range(0, 11);
@@ -42,26 +83,33 @@ public class ProblemManager : MonoBehaviour
 
             displayProblem.text = $"{num1} - {num2}";
         }
+        */
     }
 
     public void EnterAnswer()
     {
-        if(problemsSolved <= 5)
+        if(answer.text != null)
         {
-            if (int.Parse(answer.text) == (num1 + num2))
+            if(int.Parse(answer.text) == curAnswer)
             {
-                problemsSolved++;
-                GenerateProblem();
-                answer.text = "";
-            }
-        }
-        if (problemsSolved <= 10)
-        {
-            if (int.Parse(answer.text) == (num1 - num2))
-            {
-                problemsSolved++;
-                GenerateProblem();
-                answer.text = "";
+                problemsSolved += 1;
+                answer.text = null;
+                curAnswer = 0;
+                if(problemsSolved <= 5)
+                {
+                    GenerateProblem(2, 11, 0, 0);
+                }
+                else if(problemsSolved <= 10)
+                {
+                    GenerateProblem(2, 11, 0, 1);
+                }
+                else
+                {
+                    int num1 = Random.Range(1, 4);
+                    int num2 = Random.Range(0, 21);
+                    int num3 = Random.Range(0, 2);
+                    GenerateProblem(num1, num2, 0, num3);
+                }
             }
         }
     }
